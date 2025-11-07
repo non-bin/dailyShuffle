d7433eb23653619c99baebcb85b85a071b6270e7
Alice Jacka <jacka.alice@gmail.com>
2025-11-07 19:49:31 +1100

    redirect url in getInitialAccessToken was not using the env var

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -137,7 +137,7 @@ async function getInitialAccessToken(verifier: string, code: string): Promise<t.
   params.append('client_id', CLIENT_ID);
   params.append('grant_type', 'authorization_code');
   params.append('code', code);
-  params.append('redirect_uri', 'http://127.0.0.1:5173/callback');
+  params.append('redirect_uri', REDIRECT_URL);
   params.append('code_verifier', verifier);
 
   const result = await fetch('https://accounts.spotify.com/api/token', {
