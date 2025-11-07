e1e6cd7865b31254ede18154628cbb4ee8669346
Alice Jacka <jacka.alice@gmail.com>
2025-11-07 14:45:22 +1100

    factor auth scope to clobal, and add 'playlist-modify-public'

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,8 @@ import * as api from './api';
 import * as db from './db';
 import * as t from './types';
 
+const SCOPE = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
+
 const CLIENT_ID = process.env.SPOTIFY_API_CLIENT_ID || '';
 if (CLIENT_ID.length === 0) {
   throw new Error('Please set SPOTIFY_API_CLIENT_ID');
@@ -24,7 +26,7 @@ export async function redirectToAuth(req: Bun.BunRequest) {
   params.append('client_id', CLIENT_ID);
   params.append('response_type', 'code');
   params.append('redirect_uri', 'http://127.0.0.1:5173/callback');
-  params.append('scope', 'user-read-private user-read-email playlist-modify-private');
+  params.append('scope', SCOPE);
   params.append('code_challenge_method', 'S256');
   params.append('code_challenge', challenge);
 
