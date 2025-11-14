2f654da26a63dde752846baa5f2910a3d1959c63
Alice Jacka <jacka.alice@gmail.com>
2025-11-14 21:47:14 +1100

    skip-worktree build/ui.js

--- build/ui.js
+++ build/ui.js
@@ -1,1 +1,3 @@
-// To keep the compiler happy
+// This file needs to exist to keep the compiler happy if it hasn't been generated yet
+// I ran `git update-index --skip-worktree build/ui.js` to ignore changes but keep it in the directory
+// To commit changes first run `git update-index --no-skip-worktree build/ui.js`
